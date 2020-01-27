06f45e9b3b9907aa20083438c1a7505acf2f648a
James Rodriguez <jamesr817@gmail.com>
2020-01-27 05:23:05 -0600

    Got roped into installing 'DB Navigator' plug in. Took a little time to setup, and I pray it doesn't mess anything up if I close eveything out and come back later. In the meantime, I've added the appropriate annotations to the 'AbstractEntity' model class and completed the first SQL task for part 1 listed in the assignment instructions. Moving on to part 2 step 'Models' section of assignment.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,8 @@
 ## Part 1: Test it with SQL
+SELECT* FROM techjobs.job;
 
-## Part 2: Test it with SQL
+--## Part 2: Test it with SQL
 
-## Part 3: Test it with SQL
+--## Part 3: Test it with SQL
 
-## Part 4: Test it with SQL
+--## Part 4: Test it with SQL
