af40a9dcd83dca5f93a4211eac445d0026f01e54
James Rodriguez <jamesr817@gmail.com>
2020-01-27 06:15:03 -0600

    Added repository interfaces for both 'Employer' and 'Skill' model classes. Also filled in appropriate code for both model classes. 'models' and 'data layer' steps of part 2 completed. Moving on.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,33 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Please provide Job location.")
+    @Size(min = 30, max = 255, message = "Location must be only 30-255 characters.  Please try again.")
+    private String location;
 
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return location;
+    }
 }
